e51508bfa22f7a2e3ea081011d296386dbfb9b05
anderson.santos <anderson.santos@capgemini.com>
2021-07-22 11:08:16 -0300

    Início

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,12 +1,30 @@
-import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
+import setText, { appendText, showWaiting, hideWaiting } from "./results.mjs";
 
 export function get() {
+    axios.get("http://localhost:3000/orders/1")
+        .then(({ data }) => {
+            setText(JSON.stringify(data));
+        });
 }
 
 export function getCatch() {
+    axios.get("http://localhost:3000/orders/123")
+        .then(({ result }) => {
+            setText(JSON.stringify(result.data));
+        })
+        .catch(err => setText(err));
 }
 
 export function chain() {
+    axios.get("http://localhost:3000/orders/1")
+        .then(({ data }) => {
+            return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+        })
+        .then(
+            ({ data }) => {
+                setText(`City:${data.city}`);
+            }
+        );
 }
 
 export function chainCatch() {
